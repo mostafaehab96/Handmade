df03be36ac1d0728967a55f01536e436b511b681
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-09-05 11:10:15 +0300

    Change project sturcture, add user singup functionality and hashing using werkzeug

--- models/user.py
+++ models/user.py
@@ -5,13 +5,15 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from hashlib import md5
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash
 
 
-class User(BaseModel, Base):
+class User(BaseModel, Base, UserMixin):
     __tablename__ = "users"
     name = Column(String(128))
     email = Column(String(128), unique=True)
-    password = Column(String(60), nullable=False)
+    password = Column(String(128), nullable=False)
     address = Column(String(128))
     postal_code = Column(String(10))
     about = Column(String(1024))
@@ -22,7 +24,7 @@ class User(BaseModel, Base):
     def __setattr__(self, name, value):
         """sets a password with md5 encryption"""
         if name == "password":
-            value = md5(value.encode()).hexdigest()
+            value = generate_password_hash(value, salt_length=8, method='pbkdf2:sha256')
         super().__setattr__(name, value)
 
 
--- web_flask/main.py
+++ web_flask/main.py
@@ -1,26 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash
 from models import storage
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, EmailField, PasswordField
 from flask_ckeditor import CKEditor, CKEditorField
-from wtforms.validators import DataRequired, URL, Email
+from wtforms.validators import DataRequired, URL, Email, EqualTo
+from werkzeug.security import check_password_hash
+from flask_login import LoginManager, login_user, login_manager, logout_user
+from models.user import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 ckeditor = CKEditor(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return storage.get("User", user_id)
 
 
 class SignupForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     email = EmailField('Email', validators=[DataRequired(), Email()])
-    email_confirm = EmailField('Confirm Email', validators=[DataRequired(), Email()])
+    email_confirm = EmailField('Confirm Email',
+                               validators=[DataRequired(), Email(), EqualTo('email', 'Email mismatch')])
     password = PasswordField('Password', validators=[DataRequired()])
-    password_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+    password_confirm = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password', 'Password mismatch')])
     address = StringField('Address', validators=[DataRequired()])
     postal_code = StringField('Postal Code', validators=[DataRequired()])
-    about = CKEditorField('About', validators=[DataRequired()])
+    about = StringField('About', validators=[DataRequired()])
     confirm = SubmitField('Confirm')
 
 
@@ -43,9 +55,27 @@ def show_product(product_id):
     return render_template('product_details.html', product=product)
 
 
-@app.route('/signup')
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
     sign_form = SignupForm()
+
+    if sign_form.validate_on_submit():
+        name = sign_form.name.data
+        email = sign_form.email.data
+        password = sign_form.password.data
+        address = sign_form.address.data
+        postal_code = sign_form.postal_code.data
+        about = sign_form.about.data
+        user = User(name=name,
+                    email=email,
+                    password=password,
+                    address=address,
+                    postal_code=postal_code,
+                    about=about
+                    )
+        user.save()
+        login_user(user)
+        return redirect(url_for('home'))
 
     return render_template('signup.html', form=sign_form)
 
@@ -54,6 +84,7 @@ def signup():
 def login():
     login_form = LoginFrom()
     return render_template('login.html', form=login_form)
+
 
 if __name__ == "__main__":
     app.run()
